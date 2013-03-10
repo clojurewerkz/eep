992329f2e8e0ba5abab500922778174bcfeed922
Michael S. Klishin <michael@novemberain.com>
2013-03-10 15:40:04 +0400

    Change artifact name

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clojurewerkz/eep-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojurewerkz/eep "0.1.0-SNAPSHOT"
+  :description "Embedded Event Processing in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
