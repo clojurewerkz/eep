04d1cf2737dfa03278c828ef19e8ba1905549505
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-04-07 18:14:10 +0200

    Correct snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/eep "0.2.0-SNAPSHOT"
+(defproject clojurewerkz/eep "0.3.0-SNAPSHOT"
   :description "Embedded Event Processing in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
