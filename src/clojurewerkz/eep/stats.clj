622d8b97be23f595922fc967db4b66f0d6ab5cce
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-05-06 22:51:16 +0200

    Add default aggregation functions: variance and percentage

--- src/clojurewerkz/eep/stats.clj
+++ src/clojurewerkz/eep/stats.clj
@@ -10,3 +10,16 @@
   [vals]
   (let [non-nil (map identity vals)]
     (/ (reduce + non-nil) (count non-nil))))
+
+(defn variance
+  "Calculates variance, deviation from man value"
+  [arr]
+  (let [mean (/ (reduce + arr) (count arr))
+        sqr #(* % %)]
+    (/
+     (reduce + (map #(sqr (- % mean)) arr))
+     (- (count arr) 1))))
+
+(defn percentage
+  [total value]
+  (* (/ value total) 100.0))
