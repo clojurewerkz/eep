e58eb7173f83f2f741362b4327e3b0019ddc269d
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-05-31 22:22:21 +0200

    Release v0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/eep "0.2.2-SNAPSHOT"
+(defproject clojurewerkz/eep "0.3.0"
   :description "Embedded Event Processing in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
