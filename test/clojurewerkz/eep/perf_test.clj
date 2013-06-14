b23b7efc1f0f778d3e46e07d27554880a968d85a
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-06-15 00:56:02 +0200

    Extend perf tests

--- test/clojurewerkz/eep/perf_test.clj
+++ test/clojurewerkz/eep/perf_test.clj
@@ -1,16 +1,21 @@
 (ns clojurewerkz.eep.perf-test
   (:use clojure.test
+        clojurewerkz.eep.test-utils
         clojurewerkz.eep.emitter)
   (:require [clojurewerkz.eep.stats :as stats]
             [clojurewerkz.eep.windows :as windows]
             [clojurewerkz.eep.clocks :as clocks]))
 
-(deftest perf-test
-  (let [emitter (new-emitter)]
-    (defaggregator emitter :count (fn [a b]
-                                    (+ a b)) 0)
-    (time
-     (dotimes [i 2000000]
-       (notify emitter :count 1)))
+(deftest ^:perf perf-test
+  (doseq [dt [:event-loop :thread-pool :ring-buffer]]
+    (let [iterations 200
+          emitter    (new-emitter :dispatcher-type dt)
+          latch      (make-latch iterations)]
+      (defaggregator emitter :count (wrap-countdown latch +) 0)
+      (println "Dispatcher type: " dt)
+      (time
+       (dotimes [i iterations]
+         (notify emitter :count 1)))
 
-    (is (= 2000000 (state (get-handler emitter :count))))))
+      (after-latch latch
+                   (is (= iterations (state (get-handler emitter :count))))))))
