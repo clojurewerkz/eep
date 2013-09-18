aaa5c32183a9d1c86682b582735119a727321f7c
Alex Petrov <alexp@coffeenco.de>
2013-09-18 11:42:25 +0200

    Release 1.0.0-alpha5

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clojurewerkz/eep "1.0.0-alpha5-SNAPSHOT"
+(defproject clojurewerkz/eep "1.0.0-alpha5"
   :description "Embedded Event Processing in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.ifesdjeen/utils "0.4.0"]
-                 [clojurewerkz/meltdown "1.0.0-alpha3"]
+                 [clojurewerkz/meltdown "1.0.0-alpha4"]
                  [rhizome "0.1.9"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
