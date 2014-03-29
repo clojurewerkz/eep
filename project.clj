fbddb805afc864d61f549056dc62d2fba04253e3
Michael Klishin <michael@clojurewerkz.org>
2014-03-29 22:25:50 +0400

    1.5 is an extra profile now

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
                  [clojurewerkz/meltdown "1.0.0-alpha4"]
                  [rhizome "0.1.9"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:plugins [[codox "0.6.6"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases  {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master"]}
+  :aliases  {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
