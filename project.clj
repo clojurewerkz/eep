ca36f02fb39623e002e011d21aad6e89c9fa5cff
Michael Klishin <michael@clojurewerkz.org>
2015-01-02 03:15:36 +0300

    Drop 1.5 profile

--- project.clj
+++ project.clj
@@ -6,13 +6,12 @@
                  [com.ifesdjeen/utils "0.4.0"]
                  [clojurewerkz/meltdown "1.0.0"]
                  [rhizome "0.2.0"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases  {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
+  :aliases  {"all" ["with-profile" "dev:dev,1.7:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
