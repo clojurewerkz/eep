ad5d5291ddebf244b0b91544ee764e9fce27222d
Michael Klishin <michael@clojurewerkz.org>
2015-01-07 01:40:25 +0300

    Meltdown 1.1.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.ifesdjeen/utils "0.4.0"]
-                 [clojurewerkz/meltdown "1.0.0"]
+                 [clojurewerkz/meltdown "1.1.0"]
                  [rhizome "0.2.0"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
